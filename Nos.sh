4e16def0cb19c215f574b133c0ff59e3eaac15bf
Biohazard <rowdysayan99@gmail.com>
2017-12-20 23:21:34 +0530

    Update

--- Nos.sh
+++ Nos.sh
@@ -1,4 +1,3 @@
-cd ha*/q*
 grep -rl '$(call project-path-for,qcom-media)' ./ | xargs sed -i 's/$(call project-path-for,qcom-media)/hardware\/qcom\/media/g'
 grep -rl '$(call project-path-for,qcom-audio)' ./ | xargs sed -i 's/$(call project-path-for,qcom-audio)/hardware\/qcom\/audio/g'
 grep -rl '$(call project-path-for,qcom-display)' ./ | xargs sed -i 's/$(call project-path-for,qcom-display)/hardware\/qcom\/display/g'
